1f5ca71ca92c1cb87ad0fb46eca3a514411d3e1c
JFPSDEV <jfps.dev@gmail.com>
2023-11-01 09:08:56 +0100

    first commit

---
+++ src/main/java/com/example/sharedrawingapi/ShareDrawingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharedrawingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareDrawingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareDrawingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sharedrawingapi/ShareDrawingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharedrawingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareDrawingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
